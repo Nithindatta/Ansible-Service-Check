6b8c6a9af15494de8c5c56ccf8466e5f22cad376
Nithindatta <Nithindatta1421@gmail.com>
2026-06-22 12:01:16 +0000

    Ansible Pipeline

---
+++ ansible-pipeline.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    options {
+        skipDefaultCheckout(true)
+    }
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch : 'main',
+                    credentialsId: 'git',
+                    url: 'git@github.com:Nithindatta/Ansible-Service-Check.git'
+            }
+            }
+        
+        stage('Syntax Check') {
+            steps {
+                withCredentials([
+                    file(credentialsId: 'vault.pass', variable: 'VAULT_FILE'),
+                    sshUserPrivateKey(
+                        credentialsId: 'ansible-ssh-key'
+                        keyFileVariable: 'SSH_KEY'
+                        usernameVariable: 'SSH_USER'
+                    ),
+                    string (credentialsId: 'gmail-username', variable: 'GMAIL_USER')
+                    string (credentialsId: 'gmail-password', variable: 'GMAIL_PASS')
+                ]) {
+                    sh '''
+                    ansible --version
+
+                    ansible-playbook -i inventory.ini check-service.yaml --syntax-check \
+                    --vault-password-file $VAULT_FILE
+                    '''
+                }
+            }
+        }
+        }
+    }
