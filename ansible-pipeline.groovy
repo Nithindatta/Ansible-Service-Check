813074aa948e457662638d6d0fc59ddb9b21d911
Nithindatta <Nithindatta1421@gmail.com>
2026-06-22 12:41:53 +0000

    Updated

--- ansible-pipeline.groovy
+++ ansible-pipeline.groovy
@@ -2,39 +2,41 @@ pipeline {
     agent any
 
     environment {
-	ANSIBLE_COLLECTION_PATH = '/home/svc_install/.ansible/collections'
+        ANSIBLE_COLLECTIONS_PATH = '/home/svc_install/.ansible/collections'
     }
 
     options {
         skipDefaultCheckout(true)
     }
-    stage('Debug') {
-    	steps {
-            sh '''
-            whoami
-            pwd
 
-	    echo "=== Ansible Version ==="
-	    ansible --version
+    stages {
 
-	    echo "=== Collection Path ==="
-	    env | grep ANSIBLE
+        stage('Debug') {
+            steps {
+                sh '''
+                whoami
+                pwd
 
-            echo "=== Mail Module Test ==="
-            ansible-doc community.general.mail
-            '''
+                echo "=== Ansible Version ==="
+                ansible --version
+
+                echo "=== Collection Path ==="
+                env | grep ANSIBLE
+
+                echo "=== Mail Module Test ==="
+                ansible-doc community.general.mail
+                '''
             }
         }
 
-    stages {
         stage('Checkout Code') {
             steps {
-                git branch : 'main',
+                git branch: 'main',
                     credentialsId: 'git',
                     url: 'git@github.com:Nithindatta/Ansible-Service-Check.git'
             }
-            }
-        
+        }
+
         stage('Syntax Check') {
             steps {
                 withCredentials([
@@ -44,17 +46,18 @@ pipeline {
                         keyFileVariable: 'SSH_KEY',
                         usernameVariable: 'SSH_USER'
                     ),
-                    string (credentialsId: 'gmail-username', variable: 'GMAIL_USER'),
-                    string (credentialsId: 'gmail-password', variable: 'GMAIL_PASS')
+                    string(credentialsId: 'gmail-username', variable: 'GMAIL_USER'),
+                    string(credentialsId: 'gmail-password', variable: 'GMAIL_PASS')
                 ]) {
                     sh '''
-                    ansible --version
-
-                    ansible-playbook -i inventory.ini check_services.yaml --syntax-check \
-                    --vault-password-file $VAULT_FILE
+                    ansible-playbook \
+                      -i inventory.ini \
+                      check_services.yaml \
+                      --syntax-check \
+                      --vault-password-file $VAULT_FILE
                     '''
                 }
             }
         }
-        }
     }
+}
